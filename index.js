ed8c92e60be87649674b9e423a30a023f4c992d2
Bruno Barreto <bruno@brunobarreto.net>
2018-10-23 00:49:33 -0300

    Changing static folder and adding build script

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'frontend/build')))
   .use('/api/countries', require('./apiCountries'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
