47b911648afd6308df95e26820d3d44a5158182d
Bruno Barreto <bruno@brunobarreto.net>
2018-10-23 11:16:18 -0300

    Enabling CORS

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'frontend/build')))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+  })
   .use('/api/countries', require('./apiCountries'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
